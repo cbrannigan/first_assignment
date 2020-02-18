1cf9209505db6bc4742d33b9a90955a510249184
cbrannigan <corybrannigan@gmail.com>
2020-02-18 14:28:34 -0500

    updated script with function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-lines=$( ls | wc -l )
-correct=0
+function guessing {
+    lines=$( ls | wc -l )
+    correct=0
 
-while [[ $correct -eq 0 ]]
-do
-    echo "How many files are in the current directory?"
-    read guess
-    if [[ $guess -lt $lines ]]
-    then
-    echo "Your guess was too low"
-    elif [[ $guess -gt $lines ]]
-    then
-    echo "Your guess was too large"
-    else
-    echo "Good job"
-    let correct=$correct+1
-    fi
-done
+    while [[ $correct -eq 0 ]]
+    do
+        echo "How many files are in the current directory?"
+        read guess
+        if [[ $guess -lt $lines ]]
+        then
+        echo "Your guess was too low"
+        elif [[ $guess -gt $lines ]]
+        then
+        echo "Your guess was too large"
+        else
+        echo "Good job"
+        let correct=$correct+1
+        fi
+    done
+    exit
+}
+
+guessing
